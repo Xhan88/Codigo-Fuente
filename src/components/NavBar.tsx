de7323b019ab1b1a4d618ca8d3c053c7a6545bf2
Christian Martinez <dantedeat@hotmail.com>
2025-09-17 22:52:20 -0600

    navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 
 const NavBar = () => {
   return (
     <>
-    <div className="flex justify-around space-x-10 items-center mx-auto">
-          <img className="w-32" src="200.png" alt="icon-image" />
-          <div>
-              <ul className="flex justify-center space-x-8">
+    <div className="flex justify-between px-12 max-w-[1400px] items-center mx-auto">
+          <img className="w-20 cursor-pointer" src="200.png" alt="icon-image" />
+          <div className="flex space-x-[8rem]">
+              <ul className="flex justify-center space-x-[5rem] cursor-pointer">
               <li>Inicio</li>
               <li>Sobre nosotros</li>
               <li>Paginas</li>
               <li>Contacto</li>     
               </ul>
-          </div>
-          <div className="">
-            <button>F</button>
-            <button>I</button>
+
+                       <div className="">
+                       <FontAwesomeIcon icon={faFacebook} className="text-blue-600 text-xl cursor-pointer" />
+                       <FontAwesomeIcon icon={faInstagram} className="text-purple-600 text-xl cursor-pointer mx-4" />
           </div>
           <div>
-            <button className="bg-blue-500 text-white p-2 rounded-lg">Get Started</button>
+            <button className="bg-transparent-500 text-black border-2 p-2 rounded-[20px]">Contactanos</button>
           </div>
+          </div>
+ 
     </div>
 
     
