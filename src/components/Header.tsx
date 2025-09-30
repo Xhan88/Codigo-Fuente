fc1cbce81baecefb9d1cd3e6e0f213488c023a52
Christian Martinez <dantedeat@hotmail.com>
2025-09-30 06:44:20 -0600

    change AOS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import AOS from "aos"
+import "aos/dist/aos.css"; 
+
 
 const Header: React.FC = () => {
+
+   
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+    AOS.refresh(); // ✅ Ensures animations work even after dynamic content updates
+  }, []);
+
   return (
     <header className="relative bg-[rgb(24,18,146)] text-white overflow-hidden">
       <NavBar />
 
-      {/* Contenedor principal */}
-      <div className="mt-10 max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between px-6 py-20 gap-3 md:gap-6">
+      <div className="mt-20 max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between px-6 py-20 gap-10">
         
-        {/* Sección de texto */}
         <div className="flex flex-col items-center md:items-start text-center md:text-left max-w-xl">
-          <h1 className="text-4xl sm:text-5xl font-bold mb-6 leading-tight text-[#c8ed4e]">
+          <h1 className="text-4xl sm:text-5xl font-bold mb-6 leading-tight text-[#c8ed4e]" data-aos="fade-up">
             "Código Fuente" <br /> Tu agencia de Marketing digital
           </h1>
 
-          <p className="text-lg mb-8 max-w-lg">
+          <p className="text-lg mb-8 max-w-lg" data-aos="fade-up">
             Construimos lo que tu negocio necesita para crecer.  
             <span className="block mt-2">Transformamos ideas en resultados digitales.</span>
           </p>
 
-          {/* Botones */}
           <div className="flex flex-col sm:flex-row gap-4">
             <button className="bg-[#b2dc28] text-[rgb(24,18,146)] font-semibold px-6 py-3 rounded-full shadow-md hover:bg-gray-100 transition">
               Start a Project
@@ -32,18 +39,23 @@ const Header: React.FC = () => {
           </div>
         </div>
 
-        {/* Animación Lottie */}
-        <div className=" w-[500px] sm:w-[550px] md:w-[500px] lg:w-[1100px] flex justify-center">
+        <div className="relative flex justify-center items-center w-full md:w-1/2 overflow-visible scale-[1.5]">
   <DotLottieReact
     src="https://lottie.host/01326b74-3dce-49b4-b4b0-0f42b07f57c3/zSLbdfdh9T.lottie"
     loop
     autoplay
+    style={{
+      width: "100%",
+      maxWidth: "600px",
+      height: "auto",
+    }}
+    
+    data-aos="fade-left"
   />
 </div>
 
       </div>
 
-      {/* Curva inferior */}
       <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none">
         <svg
           className="relative block w-full h-32"
